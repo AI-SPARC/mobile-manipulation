ba19a512a1058188b429ea9662616c7cf8d54a38
Momesso1 <momessoalves@gmail.com>
2025-10-16 17:57:33 -0300

    Configuração do mapa com vários objetos e esteira circular adicionada.

--- isaacsim_moveit/launch/launch_isaac_sim.launch.py
+++ isaacsim_moveit/launch/launch_isaac_sim.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     gui = os.path.join(
         get_package_share_directory("isaacsim_moveit"),
         "maps",
-        "denso_welding_with_conveyor.usd",
+        "denso_welding_with_circular_conveyor_belt.usd",
     )
 
     return LaunchDescription(
