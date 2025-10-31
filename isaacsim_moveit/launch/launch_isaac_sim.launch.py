d24eaf3c85a345d2b71084402e097323a755bc9f
Momesso1 <momessoalves@gmail.com>
2025-10-31 09:35:55 -0300

    Mapa no Isaac Sim para manipulação móvel com o Franka com garra.

--- isaacsim_moveit/launch/launch_isaac_sim.launch.py
+++ isaacsim_moveit/launch/launch_isaac_sim.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     gui = os.path.join(
         get_package_share_directory("isaacsim_moveit"),
         "maps",
-        "pick_and_place_with_generated_cubes.usd",
+        "mobile_manipulation_with_panda_hand.usd",
     )
 
     return LaunchDescription(
