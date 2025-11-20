08fb38fd204fe18a2afbd46f89d292ad04315d88
Momesso1 <momessoalves@gmail.com>
2025-11-20 13:54:53 -0300

    Action Controller adicionada ao nó servidor.

--- mobile_manipulation/src/server_node.cpp
+++ mobile_manipulation/src/server_node.cpp
@@ -19,6 +19,7 @@
 
 #include "mobile_manipulation_interfaces/action/pick_object.hpp"
 #include "mobile_manipulation_interfaces/action/path.hpp"
+#include "mobile_manipulation_interfaces/action/controller.hpp"
 
 class ServerNode : public rclcpp::Node 
 {
@@ -39,7 +40,11 @@ public:
         path_client = rclcpp_action::create_client<mobile_manipulation_interfaces::action::Path>(
             this, "path");
 
-        if(!yaml_file.empty()) {
+        controller_client = rclcpp_action::create_client<mobile_manipulation_interfaces::action::Controller>(
+            this, "controller");
+
+        if(!yaml_file.empty()) 
+        {
             loadLocationsFromYaml(yaml_file);
         }
     } 
@@ -52,6 +57,7 @@ private:
     // Action clients.
     rclcpp_action::Client<mobile_manipulation_interfaces::action::PickObject>::SharedPtr client_ptr_;
     rclcpp_action::Client<mobile_manipulation_interfaces::action::Path>::SharedPtr path_client;
+    rclcpp_action::Client<mobile_manipulation_interfaces::action::Controller>::SharedPtr controller_client;
 
     std::string yaml_file;
     std::unordered_set<std::string> pick_and_place_poses;
@@ -102,6 +108,7 @@ private:
                 pose.orientation = det.bbox.center.orientation;
 
                 send_goal(id, pose);
+                send_path_goal(pose);
 
                 break;
             }
@@ -129,6 +136,7 @@ private:
         
         send_goal_options.goal_response_callback = std::bind(&ServerNode::path_goal_response_callback, this, std::placeholders::_1);
         send_goal_options.result_callback = std::bind(&ServerNode::path_result_callback, this, std::placeholders::_1);
+        send_goal_options.feedback_callback = std::bind(&ServerNode::path_feedback_callback, this, std::placeholders::_1, std::placeholders::_2);
 
         this->path_client->async_send_goal(goal_msg, send_goal_options);
     }
@@ -142,10 +150,6 @@ private:
             RCLCPP_WARN(this->get_logger(), "O servidor avisou: Recalculando rota...");
         }
 
-        RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000, 
-            "Feedback recebido. Stop Pose: [x: %.2f, y: %.2f]", 
-            feedback->stop_pose.position.x, 
-            feedback->stop_pose.position.y);
     }
 
     void path_goal_response_callback(const std::shared_ptr<rclcpp_action::ClientGoalHandle<mobile_manipulation_interfaces::action::Path>> & goal_handle)
@@ -178,7 +182,61 @@ private:
         }
     
         mobile_robot_path = result.result->path;
+    }
 
+    // Action client (controller).
+
+    void send_controller_goal(const geometry_msgs::msg::Pose & target_pose)
+    {
+        if (!this->controller_client->wait_for_action_server(std::chrono::seconds(5))) 
+        {
+            RCLCPP_ERROR(this->get_logger(), "Action server not available");
+            return;
+        }
+
+        auto goal_msg = mobile_manipulation_interfaces::action::Controller::Goal();
+        
+        goal_msg.pose = target_pose;
+
+        RCLCPP_INFO(this->get_logger(), "Enviando Goal (Pose) para Action...");
+
+        auto send_goal_options = rclcpp_action::Client<mobile_manipulation_interfaces::action::Controller>::SendGoalOptions();
+        
+        send_goal_options.goal_response_callback = std::bind(&ServerNode::controller_goal_response_callback, this, std::placeholders::_1);
+        send_goal_options.result_callback = std::bind(&ServerNode::controller_result_callback, this, std::placeholders::_1);
+
+        this->controller_client->async_send_goal(goal_msg, send_goal_options);
+    }
+
+    void controller_goal_response_callback(const std::shared_ptr<rclcpp_action::ClientGoalHandle<mobile_manipulation_interfaces::action::Controller>> & goal_handle)
+    {
+        if (!goal_handle) 
+        {
+            RCLCPP_ERROR(this->get_logger(), "Goal foi rejeitado pelo servidor");
+        } 
+        else 
+        {
+            RCLCPP_INFO(this->get_logger(), "Goal aceito, executando...");
+        }
+    }
+
+    void controller_result_callback(const rclcpp_action::ClientGoalHandle<mobile_manipulation_interfaces::action::Controller>::WrappedResult & result)
+    {
+        switch (result.code) 
+        {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+                return;
+            case rclcpp_action::ResultCode::CANCELED:
+                RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+                return;
+            default:
+                RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+                return;
+        }
+    
     }
 
     // Action client (pick_object).
